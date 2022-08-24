8fbe4fd10352af3f6f69dcc48a3cd6e5c28b4357
Amir Rezvani <106575880+AREZVANI95@users.noreply.github.com>
2022-08-24 00:15:40 -0400

    Update index.js
    Added one of many questions that the generator will soon ask the user.

--- Develop/index.js
+++ Develop/index.js
@@ -4,18 +4,33 @@ const fs = require('fs');
 const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
-    "Title of README: ",
-    "Description of README: ",
-    "License of Application: ",
-    "Contribution: ",
-    "Report Bugs: "
+    // Project Title
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project Title: (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('You need to enter a title to continue!');
+                return false;
+            }
+        }
+    },
+
 ];
 
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
